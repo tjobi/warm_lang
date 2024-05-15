394a50077c12aeb44aef39ea4040ac80edf0cda8
tjobi <tobias@brund.info>
2024-05-15 18:49:42 +0200

    remove Current from TextWindow class

--- WarmLangLexerParser/Read/TextWindow.cs
+++ WarmLangLexerParser/Read/TextWindow.cs
@@ -5,7 +5,6 @@ public abstract class TextWindow
     public int Column { get; protected set; }
     public int Line { get; protected set; }
 
-    public char Current { get; protected set;}
     public abstract bool IsEndOfFile { get; }
 
     public abstract void AdvanceText();
