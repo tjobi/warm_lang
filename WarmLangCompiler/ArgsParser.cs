75184c0b5dbe690c53f0e37e7bbb4a8947ec28b4
tjobi <tobias@brund.info>
2024-05-15 17:23:27 +0200

    make Program.cs a little more terminal friendly :D

---
+++ WarmLangCompiler/ArgsParser.cs
@@ -0,0 +1,75 @@
+namespace WarmLangCompiler;
+
+public record struct ParsedArgs(string Program, bool ParserDebug, bool LexerDebug, bool TraceExceptions);
+
+public static class ArgsParser
+{
+    private static void Help()
+    {
+        Console.WriteLine("Usage:");
+        Console.WriteLine("\tprogram        - path to a .test file to lex, parse, and evaluate");
+        Console.WriteLine("\t--lex-debug    - enables debug information for lexer");
+        Console.WriteLine("\t--parser-debug - enables debug information for parser");
+        Console.WriteLine("\t--trace        - prints a stacktrace if an exception goes uncaught");
+    }
+
+    public static ParsedArgs? ParseArgs(string[] args, string defaultProgram)
+    {
+        var program = defaultProgram;
+        var lexerDebug = false;
+        var longExceptions = false;
+        var parserDebug = false;
+
+        var isLookingForFile = true;
+        foreach (var arg in args)
+        {
+            switch(arg)
+            {
+                case "-lh": //short for "lang help" otherwise it is caught by "dotnet run"
+                case "--lang-help": {
+                    Help();
+                    return null;
+                }
+                case "--lex-debug":
+                {
+                    lexerDebug = true;
+                } break;
+                case "--parser-debug":
+                {
+                    parserDebug = true;
+                } break;
+                case "--trace":
+                {
+                    longExceptions = true;
+                } break;
+                default: {
+                    if(File.Exists(arg) && isLookingForFile)
+                    {
+                        program = arg;
+                        isLookingForFile = false;
+                    } else 
+                    {
+                        Console.WriteLine($"INFO ARGS: Invalid arg \"{arg}\"");
+                        Help();
+                        return null;
+                    }
+                }break;
+            }
+        }
+        if(program == defaultProgram)
+        {
+            Console.WriteLine("INFO ARGS: No program provided, using default: \"" + defaultProgram + "\"");
+        }
+        return new ParsedArgs(program, parserDebug, lexerDebug, longExceptions);
+    }
+
+    public static void Deconstruct(this ParsedArgs args, out string program, out bool parserDebug,
+                                   out bool lexerDebug, out bool longExceptions )
+    {
+        program = args.Program;
+        parserDebug = args.ParserDebug;
+        lexerDebug = args.LexerDebug;
+        longExceptions = args.TraceExceptions;
+        return;
+    }
+}
--- WarmLangCompiler/Program.cs
+++ WarmLangCompiler/Program.cs
@@ -4,47 +4,33 @@ using WarmLangLexerParser.AST;
 using WarmLangLexerParser.Exceptions;
 using WarmLangLexerParser.Read;
 
-var program = "SyntaxTest/Minus.test";
-var lexerDebug = true;
-var longExceptions = false;
-foreach (var arg in args)
-{
-    switch(arg)
-    {
-        case "--lex-mute":
-        {
-            lexerDebug = false;
-        } break;
-        case "--trace":
-        {
-            longExceptions = true;
-        } break;
-        default: {
-            if(!File.Exists(arg))
-            {
-                Console.WriteLine("Failed: Given an non-existing filepath");
-                return 2;
-            }
-            program = arg;
-        }break;
-    }
-}
+var DEFAULT_PROGRAM = "SyntaxTest/Minus.test";
 
-var lexer = new Lexer(new FileWindow(program));
-var tokens = lexer.Lex();
-if(lexerDebug)
+var parsedArgs = ArgsParser.ParseArgs(args, DEFAULT_PROGRAM);
+if(parsedArgs is null)
 {
-    foreach(var token in tokens)
-    {
-        Console.WriteLine(token);
-    }
+    return -1;
 }
+var (program, parserDebug, lexerDebug, longExceptions) = (ParsedArgs) parsedArgs; //cast is okay-ish, we just did a null check
 
 try 
 {
+    var lexer = new Lexer(new FileWindow(program));
+    var tokens = lexer.Lex();
+    if(lexerDebug)
+    {
+        foreach(var token in tokens)
+        {
+            Console.WriteLine(token);
+        }
+    }
+
     var parser = new Parser(tokens);
     ASTNode root = parser.Parse();
-    Console.WriteLine($"Parsed:\n\t{root}");
+    if(parserDebug)
+    {
+        Console.WriteLine($"Parsed:\n\t{root}");
+    }
 
     var res = WarmLangInterpreter.Run(root);
     Console.WriteLine($"Evaluated '{program}' -> {res}");
